1b733d509f16d90ce6f33bf4d9d2db2dbe082fc0
urtzip <urtziperez@opendeusto.es>
2015-12-09 20:09:21 +0100

    Handler para Access-Control-Allow-Origin añadido

--- app.js
+++ app.js
@@ -52,6 +52,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+//Handler para peticiones de otros dominios
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // De momento no enviamos estaticos a los usuarios
 //app.use(express.static(path.join(__dirname, 'public')));
 
