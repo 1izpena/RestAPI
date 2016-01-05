527f8df6eaf9ef80bd5409025cc1d5c817a338ba
amaiav <amaiav@opendeusto.es>
2016-01-05 20:36:41 +0100

    Socket io: antes de unirse a un canal nos salimos del canal en el que pudiera estar

--- helpers/sockets.js
+++ helpers/sockets.js
@@ -8,7 +8,14 @@ module.exports = {
                 // Hacemos join al namespace asociado al canal para poder recibir los nuevos mensajes
                 socket.on('selectChannel', function (data) {
                     if (data && data.channelid) {
-                        socket.join(data.channelid);
+                        // Salimos del namespace asociado al canal si ya esta incluido en alguno
+                        var roomName = 'CH_'+data.channelid;
+                        for (var room in socket.adapter.rooms) {
+                            if ((room.indexOf('CH_') == 0) && (room != roomName))
+                                socket.leave(room);
+                        }
+
+                        socket.join(roomName);
                     }
                 });
 
