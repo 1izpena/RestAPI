0eade96dc42c10f270b03824c9ce159346e8efb5
amaiav <amaiav@opendeusto.es>
2016-01-17 19:07:05 +0100

    GET y POST de mensajes: añadir comprobacion de que el usuario esta incluido en el canal especificado

--- controllers/message.js
+++ controllers/message.js
@@ -3,6 +3,7 @@
 var Auth  = require('../helpers/authentication');
 var Message  = require('../models/message');
 var socketio  = require('../helpers/sockets');
+var chatErrors  = require('../helpers/chatErrorsHandler');
 
 
 exports.newmessage = function newmessage (request, response) {
@@ -14,26 +15,34 @@ exports.newmessage = function newmessage (request, response) {
         }
 
         if (request.params.userid == result._id) {
-            var data = request.body;
-            var check = checkNewMessageInput(data);
-            if (!check.checked) {
-                response.status(check.errorcode).json({message: check.message});
-            }
-            else {
-                data.channelid = request.params.channelid;
-                data.userid = request.params.userid;
-                Message.newMessage(data).then(function newmessage(error, result) {
-                        if (error) {
-                            response.status(error.code).json({message: error.message});
+            chatErrors.checkuserinchannel(request.params.channelid,request.params.userid)
+                .then (function (error,result) {
+                    if (error) {
+                        response.status(401).json({message: 'User not included in requested channel'});
+                    }
+                    else {
+                        var data = request.body;
+                        var check = checkNewMessageInput(data);
+                        if (!check.checked) {
+                            response.status(check.errorcode).json({message: check.message});
                         }
                         else {
-                            // Notificamos al canal que hay nuevo mensaje
-                            socketio.getIO().sockets.to('CH_'+data.channelid).emit('newMessage', result);
-                            response.json(result);
+                            data.channelid = request.params.channelid;
+                            data.userid = request.params.userid;
+                            Message.newMessage(data).then(function newmessage(error, result) {
+                                    if (error) {
+                                        response.status(error.code).json({message: error.message});
+                                    }
+                                    else {
+                                        // Notificamos al canal que hay nuevo mensaje
+                                        socketio.getIO().sockets.to('CH_' + data.channelid).emit('newMessage', result);
+                                        response.json(result);
+                                    }
+                                }
+                            );
                         }
                     }
-                );
-            }
+                });
         }
         else {
             response.status(401).json({message: 'Not authorized to post messages from another user'});
@@ -50,21 +59,28 @@ exports.getmessages = function getmessages (request, response) {
         }
 
         if (request.params.userid == result._id) {
-            var data = request.body;
-
-            data.channelid = request.params.channelid;
-            data.userid = request.params.userid;
-            data.limit = request.query.limit;
-            data.page = request.query.page;
-            Message.getMessages(data).then(function(error, result) {
+            chatErrors.checkuserinchannel(request.params.channelid,request.params.userid)
+                .then(function (error,result) {
                     if (error) {
-                        response.status(error.code).json({message: error.message});
+                        response.status(401).json({message: 'User not included in requested channel'});
                     }
                     else {
-                        response.json(result);
+                        var data = request.body;
+                        data.channelid = request.params.channelid;
+                        data.userid = request.params.userid;
+                        data.limit = request.query.limit;
+                        data.page = request.query.page;
+                        Message.getMessages(data).then(function (error, result) {
+                            if (error) {
+                                response.status(error.code).json({message: error.message});
+                            }
+                            else {
+                                response.json(result);
+                            }
+                        });
                     }
-                }
-            );
+
+                });
         }
         else {
             response.status(401).json({message: 'Not authorized to get messages from another user'});
