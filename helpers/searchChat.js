6b912203df3f8c5704666fb1de6b07b199f89343
izaskun_peke_@hotmail.com <izaskun_peke_@hotmail.com>
2016-02-19 01:18:14 +0100

    Comentada la busqueda de elastic para los campos user

--- helpers/searchChat.js
+++ helpers/searchChat.js
@@ -10,10 +10,10 @@ module.exports = function(request, response, client) {
                     query_string: {
                     	query: "(content.title:"+request.body.key+
                     		" OR content.text:"+request.body.key+
-                    		" OR content.answers._user:"+request.body.key+
+                    		//" OR content.answers._user:"+request.body.key+
                     		" OR content.answers.text:"+request.body.key+
                     		" OR content.filename:"+request.body.key+
-                    		" OR _user:"+request.body.key+
+                    		//" OR _user:"+request.body.key+
                     		") AND (_channel:"+request.body.channel+")"
                     }
 
