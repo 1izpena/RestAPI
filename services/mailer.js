fccb46597b73b4cfed0a127499f63a65cbd1a4d6
Your Name <garcia.a.asier@gmail.com>
2016-02-20 15:41:01 +0100

    quitar underline de link en mails

--- services/mailer.js
+++ services/mailer.js
@@ -19,7 +19,7 @@ switch(request.action){
 		case 0:
 
 		var html = "<img src='http://i66.tinypic.com/1z2m64k.png' style='width:350px;height:128px;'><br/>"+
-    	"<p style='color:#163B5A;'>To activate your account follow the link  <a href='http://localhost:9000/#/activate/"+request.token+"'>activate</a></p>";
+    	"<p style='color:#163B5A;'>To activate your account follow the link  <a style='color:#8d1313;text-decoration: none;' href='http://localhost:9000/#/activate/"+request.token+"'>activate</a></p>";
     	var subject = "Meanstack activate account";
 
     	break;
@@ -27,7 +27,7 @@ switch(request.action){
     	case 1:
 
     	var html = "<img src='http://i66.tinypic.com/1z2m64k.png' style='width:350px;height:128px;'><br/>"+
-    	"<p style='color:#163B5A;'>To restart your password follow the link <a href='http://localhost:9000/#/reset/"+request.token+"'>Restart Pass</a></p>";
+    	"<p style='color:#163B5A;'>To restart your password follow the link <a style='color:#8d1313;text-decoration: none;' href='http://localhost:9000/#/reset/"+request.token+"'>Restart Pass</a></p>";
     	var subject = "Meanstack restart password";
 
     	break;
@@ -36,7 +36,7 @@ switch(request.action){
 
     	var html = "<img src='http://i66.tinypic.com/1z2m64k.png' style='width:350px;height:128px;'><br/>"+
       "<p style='color:#163B5A;'> Hi " + request.username +" !! your password has been restarted"+
-		"<a href='http://localhost:9000/#/login/'>Login</a>";
+		"<a style='color:#8d1313;text-decoration: none;' href='http://localhost:9000/#/login/'>Login</a>";
 		var subject = "Meanstack password restarted";
 
 		break;
@@ -44,7 +44,7 @@ switch(request.action){
         case 3:
 
     var html = "<img src='http://i66.tinypic.com/1z2m64k.png' style='width:350px;height:128px;'><br/>"+
-      "<p style='color:#163B5A;'>Before you restart your password you must activate it<a href='http://localhost:9000/#/activate/"+request.token+"'>activate</a></p>";
+      "<p style='color:#163B5A;'>Before you restart your password you must activate it<a style='color:#8d1313;text-decoration: none;' href='http://localhost:9000/#/activate/"+request.token+"'>activate</a></p>";
       var subject = "Meanstack activate account";
 
       break;
