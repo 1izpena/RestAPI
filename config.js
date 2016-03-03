e6903218dcbbbb468baadf1341828c5c21c1e828
1izpena <1izpena@opendeusto.es>
2016-03-03 04:43:39 +0100

    prueba

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
     'expire': 4044000,
     'algorithm':'RS256',
     'salt_work_factor' : 5,
-    'database': 'mongodb://dessiuser:dessi2015@ds063134.mongolab.com:63134/dessi',
-    //'database': 'mongodb://127.0.0.1:27017/dessi',
+    //'database': 'mongodb://dessiuser:dessi2015@ds063134.mongolab.com:63134/dessi',
+    'database': 'mongodb://127.0.0.1:27017/dessi',
     'port': 3200,
     'internalUserMail': 'internalUser@localhost',
 
